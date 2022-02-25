8934620d66e4b1108049b8f20b35a73f5bf09eb5
micascheid <mica.linscheid@western.edu>
2022-02-25 15:33:50 -0700

    fix #1

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ np.set_printoptions(threshold=sys.maxsize)
 
 # nomics api key: m_419a355b4a2283777a4aa1a6590c43ffb409fe5a
 if __name__ == '__main__':
-    file_name = './test2'
+    file_name = './test1'
     npy_file = './Data/npy/'+file_name+'.npy'
     json_file = './Data/json/'+file_name
     # window = tk.Tk(screenName="test")
@@ -20,11 +20,11 @@ if __name__ == '__main__':
     # window.mainloop()
 
     # Aquire the desired data
-    # start = datetime(year=2022, month=2, day=1, hour=0, minute=0, second=0)
-    # end = datetime(year=2022, month=2, day=24, hour=23, minute=0, second=0)
-    #
-    # dg1 = DataGrab(exchange="gdax", tf="1h", market="BTC-USD", start=start, end=end, file=file_name)
-    # dg1.export_data()
+    start = datetime(year=2022, month=2, day=1, hour=0, minute=0, second=0)
+    end = datetime(year=2022, month=2, day=24, hour=23, minute=0, second=0)
+
+    dg1 = DataGrab(exchange="gdax", tf="1h", market="BTC-USD", start=start, end=end, file=file_name)
+    dg1.export_data()
 
     #start2 = datetime.strptime(input("Start time as %Y-%m-%d %H:%M:%S| "), "%Y-%m-%d %H:%M:%S")
     #2022-01-01 00:00:00
