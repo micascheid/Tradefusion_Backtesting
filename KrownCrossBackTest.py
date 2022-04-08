453101cc61fde66c99332fa6ccf0dd528d9d7851
micascheid <mica.linscheid@western.edu>
2022-04-08 17:32:51 -0600

    #27 bringing in rsi to kc_json export file plus some decimal precision

--- KrownCrossBackTest.py
+++ KrownCrossBackTest.py
@@ -7,6 +7,12 @@ from KCObj import KCObj
 MA = 13
 LOOKBACK = 252
 EMA = abstract.Function('EMA')
+PRECISION = 4
+
+
+def precision(num, prec):
+    return "{:.{}f}".format(num, prec)
+
 
 class KrownCrossBackTest:
     def __init__(self, emaL, emaM, emaH, np_data, json_data, kc_file, ticker):
@@ -14,9 +20,9 @@ class KrownCrossBackTest:
         self.emaL = emaL
         self.emaM = emaM
         self.emaH = emaH
-        self.emaLL = EMA(self.np_data, self.emaL)
-        self.emaML = EMA(self.np_data, self.emaM)
-        self.emaHL = EMA(self.np_data, self.emaH)
+        self.emaLL = [precision(x, 4) for x in EMA(self.np_data, self.emaL)]
+        self.emaML = [precision(x, 4) for x in EMA(self.np_data, self.emaM)]
+        self.emaHL = [precision(x, 4) for x in EMA(self.np_data, self.emaH)]
         self.start = datetime.strptime(np_data['meta'][0].strip("Z"), "%Y-%m-%dT%H:%M:%S")
         self.tf = np_data['meta'][1]
         self.json_data = json_data
@@ -36,7 +42,7 @@ class KrownCrossBackTest:
     #
     #     #Algo trade:
     #         #entry: After complete cross enter within 1% of 21 if oppurtunity occurs and risk to 55 ema is 2% or less
-    #         #exit: bbwap > 80 first, 9 cross 21 for sure out
+    #         #exit: bbwp > 80 first, 9 cross 21 for sure out
     #         #stop: close below emaH
     #
     #     cross_start = self.emaH*4
@@ -77,7 +83,7 @@ class KrownCrossBackTest:
     #             "cross_occurrences": cross_occurence}
 
     def ema_crosses_2(self):
-        #bbwap start:
+        #bbwp start:
         #ema start: length is same as raw_json
         cross_start = self.emaH*4
         cross_occurence = {}
@@ -154,8 +160,15 @@ class KrownCrossBackTest:
             for k in range(j-LOOKBACK, j):
                 if bbw[k] < current_bbw:
                     count += 1
-            bbwp.append("{:.3f}".format((count/LOOKBACK)*100))
+            bbwp.append((count/LOOKBACK)*100)
+        print("len bbwp:", len(bbwp))
         return bbwp
+
+    def rsi(self):
+        RSI = abstract.Function('RSI')
+        print(self.np_data['meta'])
+        rsi = RSI(self.np_data)[MA+LOOKBACK:]
+        return rsi
 
     def get_daily_trend(self):
         dt = DailyTrend(self.ticker)
@@ -174,17 +187,17 @@ class KrownCrossBackTest:
         daily_trend = dt.get_daily_trend()
         json_data = self.json_data
         # occurences = self.ema_crosses()['cross_occurrences']
-        bbwap = self.bbwp()
         emaL = self.emaLL
         emaM = self.emaML
         emaH = self.emaHL
         cross_list = self.ema_crosses_2()['cross_list']
 
         # Deterine start point in which data is useful
-            # which ever is greatest bbwap(Lookback + MA) or emaH*4
+            # which ever is greatest bbwp(Lookback + MA) or emaH*4
         ema_start = self.emaH*4
         start_time = ema_start if ema_start > LOOKBACK+MA else LOOKBACK+MA
         bbwp = self.bbwp()
+        rsi = self.rsi()
         kc_list = []
         #start = datetime(year=2022, month=1, day=1, hour=0, minute=0, second=0).isoformat()+"Z"
 
@@ -192,50 +205,37 @@ class KrownCrossBackTest:
             time = (self.json_data[x]['timestamp'])
             date_only = datetime.strptime(time.strip("Z"), "%Y-%m-%dT%H:%M:%S").replace(hour=0, minute=0,
                                                                                         second=0).isoformat() + "Z"
-            kc_dict = {"timestamp": str(self.json_data[x]['timestamp']),
-                    "close": str(self.json_data[x]['close']),
-                    "cross_status": str(cross_list[x-ema_start][1]),
-                    "bbwap": str(bbwp[(x-start_time)+1]),
-                    "emaL": str(emaL[x]),
-                    "emaM": str(emaM[x]),
-                    "emaH": str(emaH[x]),
-                    "daily_ema": daily_trend[date_only]}
+            kc_dict = {
+                "timestamp": str(self.json_data[x]['timestamp']),
+                "close": str(precision(float(self.json_data[x]['close']), 4)),
+                "cross_status": str(cross_list[x-ema_start][1]),
+                "bbwp": str(precision((bbwp[(x-start_time)+1]), PRECISION)),
+                "rsi": precision(rsi[x-start_time], PRECISION),
+                "emaL": str(emaL[x]),
+                "emaM": str(emaM[x]),
+                "emaH": str(emaH[x]),
+                "daily_ema": daily_trend[date_only]}
             kc_list.append(kc_dict)
             #start = (start + timedelta(hours=1)).isoformat()+"Z"
         file = open('./Data/kc/' + self.kc_file, "w")
         file.write(json.dumps(kc_list))
         file.close()
 
+
     def kc_load(self):
         file = open('./Data/kc/' + self.kc_file, "r")
         return json.load(file)
 
     def entry_exit(self):
-        # Returns a list of tuples(entry, exit)
-        # Algo trade:
-        #Risk Management: GTFO
-            # stop 1: close above or below 55e
-            # Expidited: above or below previous swing high/low - Note: bring in later as pivots are not calculated rn
-        #Entry:
-            # a.)
-            # b.) Can start an entry with a close around 21 if more aggressive 55 if more conservative : Roger
-            # c.) confirmation of rsi divergence : Negative
-            # d.) low or downtrending bbwp : Roger
-        #Exit: Which ever of the following comes first
-            # a.) RSI divergence : Negative
-            # b.) BBWP > 80 or 90 : Positive
-            # c.) 9e crossing 21e
-        # Invalidation:
-            # hourly trend is against daily trend or suggests caution
-            #
         # Other thoughts: Checking which regime BTC is in regarding bull market support band
         self.set_krown_cross_json_export()
         kc_data = self.kc_load()
         positions = []
+        positions_dict={}
         global entry
         global exit
         long_bias = False
-        ema_mid_tolerance = 1
+        ema_mid_tolerance = .5
         ema_high_tolerance = 2
         last_entry = ""
         last_exit_time = datetime.strptime(kc_data[0]['timestamp'].strip('Z'), "%Y-%m-%dT%H:%M:%S")
@@ -246,8 +246,8 @@ class KrownCrossBackTest:
             # convert json_obj variables back
             kc_obj = KCObj(x)
 
-            timestamp, close, cross_status, bbwap, emaL, emaM, emaH, daily_ema = kc_obj.timestamp, kc_obj.close, \
-                                                                      kc_obj.cross_status, kc_obj.bbwap, kc_obj.emaL,\
+            timestamp, close, cross_status, bbwp, emaL, emaM, emaH, daily_ema = kc_obj.timestamp, kc_obj.close, \
+                                                                      kc_obj.cross_status, kc_obj.bbwp, kc_obj.emaL,\
                                                                       kc_obj.emaM, kc_obj.emaH, kc_obj.daily_ema
             #looking for an entry
             if not in_trade:
@@ -261,7 +261,7 @@ class KrownCrossBackTest:
                     ema_low_dif = (emaL/close)-1
                     ema_mid_dif = (emaM/close)-1
                     ema_high_dif = (emaH/close)-1
-                    if ema_mid_dif <= ema_mid_tolerance and not (LIMBO in cross_status) and bbwap <= 20:
+                    if ema_mid_dif <= ema_mid_tolerance and not (LIMBO in cross_status):
                         entry = kc_obj
                         in_trade = True
                         continue
@@ -277,10 +277,19 @@ class KrownCrossBackTest:
             #looking for an exit
             #TODO looking for an exit on long
             if in_trade:
-                if not (LIMBO in cross_status) and (bbwap >= 100 or close <= emaL):
+                if not (LIMBO in cross_status) and (bbwp >= 90):# or close <= emaL):
                     # entry_time = datetime.strptime(entry.timestamp, "%Y-%m-%dT%H:%M:%S")
                     # exit_time = datetime.strptime(kc_obj.timestamp, "%Y-%m-%dT%H:%M:%S")
                     duration = (entry.timestamp - kc_obj.timestamp).seconds // 3600
+                    pnl = ((kc_obj.close/entry.close)-1)*100
+                    w_l = "w" if pnl > 0 else "l"
+                    dict_obj = {
+                        "timestamp_exit:": kc_obj.timestamp,
+                        "duration": duration,
+                        "pnl": pnl,
+                        "w_l": w_l
+                    }
+                    positions_dict[entry.timestamp] = dict_obj
                     positions.append(((entry.timestamp, entry.close), (kc_obj.timestamp, kc_obj.close), duration))
                     long_bias = False
                     in_trade = False
@@ -292,42 +301,53 @@ class KrownCrossBackTest:
 
             #TODO looking for an exit on short
 
-        return positions
+        return positions_dict
+
+    # def entry_exit_basic(self):
+    #     #
 
     def get_roi(self):
         positions = self.entry_exit()
+        position_entry_exit = []
+        # for entries in positions:
+        #     position_entry_exit.append((entries[0][0].isoformat(), entries[1][0].isoformat()))
+        # print(position_entry_exit)
         average_roi_list = []
+        print(positions)
+        sorted_positions = sorted(positions.items(), key=lambda kv: kv[1]['pnl'], reverse=True)
+        print(dict(sorted_positions))
         win_loss = []
         win = 0
         loss = 0
         capital = 1000
-        for x in positions:
-            average_roi_list.append(round((((x[1][1]/x[0][1])-1)*100), 3))
-        for y in positions:
-            if y[0][1] < y[1][1]:
-                win_loss.append("W")
-                win += 1
-            else:
-                win_loss.append("L")
-                loss += 1
+        # for x in positions:
+        #     average_roi_list.append(round((((x[1][1]/x[0][1])-1)*100), 3))
+        # for y in positions:
+        #     if y[0][1] < y[1][1]:
+        #         win_loss.append("W")
+        #         win += 1
+        #     else:
+        #         win_loss.append("L")
+        #         loss += 1
         # print(len(average_roi_list))
         # print(average_roi_list)
-        b_w = max(average_roi_list)
-        b_l = min(average_roi_list)
+        # b_w = max(average_roi_list)
+        # b_l = min(average_roi_list)
         # print("Biggest Winner =", b_w, "| Biggest Loser =", b_l)
-        average_trade = sum(average_roi_list)/len(average_roi_list)
+        # average_trade = sum(average_roi_list)/len(average_roi_list)
         # print("Trade Average: {}%".format("{:.2f}".format(average_trade)))
         # print("Percentage Win: {}%".format(("{:.2f}".format(win/(win+loss)*100))))
-        w_l = (win/(win+loss) * 100)
+        # w_l = (win/(win+loss) * 100)
 
-        for z in average_roi_list:
-            #print("trade:", capital)
-            capital = capital * (1+(z/100))
-        return {
-                "w_l": w_l,
-                "average_trade": average_trade,
-                "capitol_ending": capital
-                }
+        # for z in average_roi_list:
+        #     #print("trade:", capital)
+        #     capital = capital * (1+(z/100))
+        # return {
+        #         "w_l": w_l,
+        #         "average_trade": average_trade,
+        #         "capitol_ending": capital,
+        #         "total_trades": len(average_roi_list)
+        #         }
 
     # def __str__(self):
     #     kc = self.ema_crosses()
